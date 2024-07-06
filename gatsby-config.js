8ab8b04f7d217799ae2096696f2a5f6c10c60795
dansinger <singerde1@gmail.com>
2024-07-06 12:32:00 -0400

    Added plugin-manifest and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,18 @@ module.exports = {
         ],
         display: 'swap'
       }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`
+      },
     }
   ],
 }
